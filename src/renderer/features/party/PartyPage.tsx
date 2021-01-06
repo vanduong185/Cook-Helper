4fe610b134971de94f5aab1cda303c431c0f42bb
kysuhai <kysuhai@teneocto.io>
2021-01-06 17:46:36 +0700

    add some ui for party page

--- src/renderer/features/party/PartyPage.tsx
+++ src/renderer/features/party/PartyPage.tsx
@@ -1,15 +1,49 @@
+import {
+  Box,
+  Container,
+  createStyles,
+  Divider,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
 import React, { ReactElement } from 'react';
+import { MenuBox } from './components/MenuBox';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    headingLine: {
+      width: '30%',
+      margin: '0px 50px',
+    },
+  }),
+);
 
 export const PartyPage = (): ReactElement => {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <h1>Party</h1>
-    </div>
+    <Container>
+      <h1>Tạo tiệc</h1>
+      <Box
+        height="100"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Divider className={classes.headingLine}></Divider>
+        <h1>Thực đơn</h1>
+        <Divider className={classes.headingLine}></Divider>
+      </Box>
+
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <MenuBox></MenuBox>
+      </Box>
+    </Container>
   );
 };
---
+++ src/renderer/features/party/components/MenuBox.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import { Paper } from '@material-ui/core';
+
+export const MenuBox = (): ReactElement => {
+  return (
+    <Paper>
+      <span>Menu 1</span>
+    </Paper>
+  );
+};
